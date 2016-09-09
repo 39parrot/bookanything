c1cc615a32908c170dbd2c0b63fb2d2ce0a47bb7
dmitry <dmitry.kouznetsov@paxport.se>
2016-09-09 16:52:31 +0200

    my things screen

--- imports/ui/components/my_things.js
+++ imports/ui/components/my_things.js
@@ -1,1 +1,13 @@
-import './my_things.js'
+import './my_things.html';
+
+import { Things } from '/imports/api/things/things.js';
+
+Template.my_things.helpers({
+  things() {
+    if ( !!Meteor.user ) {
+      return Things.find( { owner: Meteor.userId() } );
+    } else {
+      return null;
+    }
+  }
+});
