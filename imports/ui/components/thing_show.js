2d5dc392d38b830cd0c569139665e429de10aa9d
dmitry <dmitry.kouznetsov@paxport.se>
2016-09-12 16:37:22 +0200

    a fix

--- imports/ui/components/thing_show.js
+++ imports/ui/components/thing_show.js
@@ -3,20 +3,11 @@ import './thing_show.html';
 import { Things } from '/imports/api/things/things.js';
 
 Template.thing_show.onCreated(function() {
-  
+
 });
 
 Template.thing_show.helpers({
   thing() {
-    return {
-      slug: "123456",
-      name: "Audi A3",
-      address: "Sankt Eriksgatan 117, Business Center Bilpalatset",
-      price: 520,
-      price_period: "w",
-      category: "cars",
-      owner: "cqMYAwyM36CCKyxu7"
-    }
-    // return Things.find( { slug: FlowRouter.getParam('thing') } );
+    return Things.findOne( { slug: FlowRouter.getParam('thing') } );
   },
 });
